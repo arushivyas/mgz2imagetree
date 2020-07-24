373e811201872bd2fe271c8f079a4c0a5043d935
Arushi Vyas <arushi@havana.lan>
2020-07-24 14:36:29 -0400

    variable instantiation

--- mgz2imagetree/mgz2imagetree.py
+++ mgz2imagetree/mgz2imagetree.py
@@ -8,3 +8,25 @@ from        pfmisc              import  other
 from        pfmisc              import  error
 import argparse
 import pftree
+
+class mgz2imagetree():
+    """ 
+    A class based on 'pftree' structure that walks through the inputdir, 
+    and filters mgz volumes for each subject into its constituent labels as indivdual directories 
+    and stores them in an outputdir structure similar to the inputdir.
+
+    """
+
+    def declare_selfvars(self):
+        """
+        A block to declare self variables
+        """
+
+        #
+        # Object desc block
+        #
+        self.str_desc                   = ''
+        self.__name__                   = "pfdicom_tagExtract"
+        self.str_version                = "2.2.20"
+
+        
